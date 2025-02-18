12b82a07d0434a80c3addb1a225f9f5c7eff1150
Nancy4510 <nancy4510@gmail.com>
2025-02-18 16:16:02 -0500

    Updating the JS file

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function getWeather() {
     const forecastUrl = `api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
 
     fetch(currentWeatherUrl)
-        .then(res => res.jsaon())
+        .then(res => res.json())
         .then(data => {
             displayWeather(data);
         })
@@ -66,5 +66,28 @@ function displayWeather(data) {
 
         showImage();
     }
+}
+
+function displayHourlyForecast(hourlyData) {
+    const hourlyForecastDiv = document.getElementById('hourly-forecast');
+    const next24Hours = hourlyData.slice(0,8);
+
+    next24Hours.forEach(item => {
+        const dateTime = new Date(item.dt * 1000);
+        const hour = dateTime.getHours();
+        const temperature = Math.round(item.main.temp - 273.15);
+        const iconCode = item.weather[0].icon;
+        const iconUrl = `https://openweathermap.org/img/wn/${iconCode}.png`;
+        
+    const hourlyItemHtml = `
+        <div class = "hourly-item">
+            <span>${hour}:00</span>
+            <img src="${iconUrl}" alt="Hourly Weather Icon">
+            <span>${temperature}</span>
+        </div>`;
+        
+    hourlyForecastDiv.innerHTML += hourlyItemHtml;
+
+    });
 }
 
